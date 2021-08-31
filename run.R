8ca43f993fd9ec1260433119087b9ee8f281e6b0
Boris <boris.lipinski83@gmail.com>
2021-08-31 14:14:30 +0200

    First

--- run.R
+++ run.R
@@ -3,7 +3,7 @@ library(shiny)
 port <- Sys.getenv('PORT')
 
 shiny::runApp(
-  appDir = setwd("/home/boris/Bureau/scShiny/shiny/"),
+  appDir = getwd(),
   host = '0.0.0.0',
   port = as.numeric(port)
 )
