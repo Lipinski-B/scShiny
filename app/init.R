d4dacf6eced6b16829130275170c8f2932d91ae9
Boris <boris.lipinski83@gmail.com>
2021-08-31 15:30:03 +0200

    test

--- app/init.R
+++ app/init.R
@@ -3,23 +3,13 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("Seurat", "monocle",
-"escape",
-"dittoSeq",
-"plotly",
-"dplyr",
-"shiny",
-"shinyjs",
-"shinybusy",
-"shinyWidgets",
-"shinydashboard",
-"dashboardthemes",
-"future")
+my_packages = c("Seurat", "monocle","escape","dittoSeq","plotly","dplyr","shiny","shinyjs","shinybusy","shinyWidgets","shinydashboard","dashboardthemes","future")
 
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
+    BiocManager::install(p)
   }
 }
 
