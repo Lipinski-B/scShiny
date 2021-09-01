2804e61327c821f3aa2ab3bbba4757c74a5eb0a4
Boris <boris.lipinski83@gmail.com>
2021-09-01 13:26:14 +0200

    Package

--- init.R
+++ init.R
@@ -3,11 +3,12 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("fastmap", "htmltools", "plotly","dplyr","shiny","shinyjs","shinybusy","shinyWidgets","shinydashboard","dashboardthemes")#"Seurat") 
+my_packages = c("plotly","dplyr","shiny","shinyjs","shinybusy","shinyWidgets","shinydashboard","dashboardthemes")#"Seurat") 
 
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
+    install_version("htmltools", version = "0.5.1", repos = "http://cran.us.r-project.org ")
     helpers.installPackages(p)
     #BiocManager::install("monocle","escape","dittoSeq")
   }
