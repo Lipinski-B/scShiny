085d939b06e875b25859ab4f506c8743da233e64
Boris <boris.lipinski83@gmail.com>
2021-09-01 15:08:56 +0200

    Package

--- global.R
+++ global.R
@@ -14,7 +14,7 @@ library(dashboardthemes)
 #library(future)
 
 source(file = "functions.R")
-load(file = "/home/boris/Documents/analyse/singlet_FL08G0293.RData")
+load(file = "singlet.RData")
 
 feature <- row.names(as.matrix(all[["RNA"]]@counts)); metadata <- c() ; List <- list()
 for(i in 1:length(colnames(all@meta.data))){if (length(levels(as.factor(all@meta.data[[i]]))) > 1 && length(levels(as.factor(all@meta.data[[i]]))) < 600 && is.numeric(levels(as.factor(all@meta.data[[1]])))==F ){metadata <- c(metadata, colnames(all@meta.data)[i])}}
