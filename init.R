0e3a9f7d1e8bd79d11cf5b36975167d0e6befebb
Boris <boris.lipinski83@gmail.com>
2021-09-01 13:35:14 +0200

    Package

--- init.R
+++ init.R
@@ -8,10 +8,11 @@ my_packages = c("plotly","dplyr","shiny","shinyjs","shinybusy","shinyWidgets","s
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install_version("htmltools", version = "0.5.1", repos = "http://cran.us.r-project.org ")
+    install.packages("htmltools", version = "0.5.1", repos = "http://cran.us.r-project.org ")
     helpers.installPackages(p)
     #BiocManager::install("monocle","escape","dittoSeq")
   }
 }
 
 invisible(sapply(my_packages, install_if_missing))
+
