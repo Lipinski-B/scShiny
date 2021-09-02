36b1de6175af4fe5b161c43ed7369cfc26d09cf0
Boris <boris.lipinski83@gmail.com>
2021-09-02 17:05:33 +0200

    packages

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
-chown $USER.$USER /var/log/shiny-server
+chmod 666 /var/log/shiny-server
 exec shiny-server >> /var/log/shiny-server.log 2>&1
