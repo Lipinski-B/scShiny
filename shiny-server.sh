9cb4780e334ea45bf5e7771dbc9ffdfdac57e40f
Boris <boris.lipinski83@gmail.com>
2021-09-03 12:48:06 +0200

    fix

--- shiny-server.sh
+++ shiny-server.sh
@@ -3,4 +3,5 @@
 P=$(id -u) ; printf "run_as u$P;" >> /etc/shiny-server/shiny-server.conf
 mkdir -p /var/log/shiny-server
 chmod 666 /var/log/shiny-server
-exec shiny-server >> /var/log/shiny-server.log 2>&1
+#exec shiny-server >> /var/log/shiny-server.log 2>&1
+shiny-server
