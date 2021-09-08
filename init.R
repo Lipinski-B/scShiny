0af5182895866f1b83f0d9ffd8b09b0b3f2159eb
Boris <boris.lipinski83@gmail.com>
2021-09-08 14:13:20 +0200

    first

--- init.R
+++ init.R
@@ -3,15 +3,15 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c('plotly','shiny','shinybusy','shinydashboard','dashboardthemes','shinyWidgets')#'Seurat',
+my_packages = c('plotly','shiny','shinybusy','shinydashboard','dashboardthemes','shinyWidgets','BiocManager')#'Seurat',
 
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages("fastmap_1.1.0.tar.gz", repos = NULL, type="source")
     install.packages("htmltools_0.5.2.tar.gz", repos = NULL, type="source")
-    install.packages("dittoSeq_1.4.1.tar.gz", repos = NULL, type="source", dependencies = T )
     install.packages(p)
+    BiocManager::install('dittoSeq', update = F, force = TRUE)
   }
 }
 
