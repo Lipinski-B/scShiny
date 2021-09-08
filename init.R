4a9a1f488fc3df7541394e4c3c579c28eff59ee5
Boris <boris.lipinski83@gmail.com>
2021-09-08 12:40:27 +0200

    test

---
+++ init.R
@@ -0,0 +1,17 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c('Seurat','plotly','shiny','shinybusy','shinydashboard','dashboardthemes','shinyWidgets')
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+    BiocManager::install("dittoSeq")
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
