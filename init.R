852247e6710671b4a13afa6af6723108d2d82e4e
Boris <boris.lipinski83@gmail.com>
2021-09-08 14:03:25 +0200

    first

--- init.R
+++ init.R
@@ -10,8 +10,8 @@ install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages("fastmap_1.1.0.tar.gz", repos = NULL, type="source")
     install.packages("htmltools_0.5.2.tar.gz", repos = NULL, type="source")
+    install.packages("dittoSeq_1.4.1.tar.gz", repos = NULL, type="source", dependencies = T )
     install.packages(p)
-    install.packages("dittoSeq_1.4.1.tar.gz", repos = NULL, type="source")
   }
 }
 
